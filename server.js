06495d8fabf0d83d3ddbe5cf9912f5ff324f80e9
Nacho <inodal@gmail.com>
2016-09-17 18:29:11 +0100

    Refactoring middleware

--- server.js
+++ server.js
@@ -2,22 +2,10 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function(req, res, next) {
-        console.log((new Date()).toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware');
+
 //Middleware MUST BE the first app.use or it will not work (WTF?)
 app.use(middleware.logger);
-
-// app.get('/', function(req, res) {
-//     res.send('Hello Express!');
-// });
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
     res.send('About us!');
